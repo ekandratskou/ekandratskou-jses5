dbc0bb691db0babf4ac40f87c50531b60874946c
Eduard Kandratskou <EKandratskou@ibagroup.eu>
2022-07-04 17:41:28 +0300

    Fixed task3

--- src/task3/task3.js
+++ src/task3/task3.js
@@ -1,14 +1,5 @@
 function isEven(N) {
-    if (N == 0) {
-        return true
-    }
-    else if (N == 1){
-        return false
-    } 
-    else if (N>0){
-        return isEven(N-2)
-    }
-    else {
-        return isEven(-N)
-    }
-} 
+    return (N==0)?  true : (N==1)? false : (N>0)? isEven(N-2) : isEven(Math.abs(N));
+    
+}
+
