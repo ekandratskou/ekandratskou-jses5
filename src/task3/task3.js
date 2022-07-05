c0dc8da2a3672273a4acee4515c8abdfb650933c
Eduard Kandratskou <EKandratskou@ibagroup.eu>
2022-07-05 10:53:32 +0300

    task3 with operator &

--- src/task3/task3.js
+++ src/task3/task3.js
@@ -1,5 +1,5 @@
-function isEven(N) {
-    return (N==0)?  true : (N==1)? false : (N>0)? isEven(N-2) : isEven(Math.abs(N));
-    
-}
+function isEven(x) {
+    return ( x & 1 ) ? false : true;
+  }
+ 
 
