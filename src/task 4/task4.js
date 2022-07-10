4f1df3655d64b1506ff0fa1eede3dc303262488f
Eduard <eduard@exampl.com>
2022-07-10 15:40:28 +0300

    rewerited task 4

--- src/task 4/task4.js
+++ src/task 4/task4.js
@@ -1,11 +1,5 @@
 function CountBs(str){
-    var count  = 0;
-    for (i=0; i<str.length; i++){
-        if (str.charAt(i)=='B') {
-            count=count+1;
-        }
-    }
-    return count
+       return CountChar(str, "B")
 }
 
 function CountChar(str, Nth){
