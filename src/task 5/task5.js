b8df4bca531dc07d1a34c8dba43fd697b3627c4d
Eduard <eduard@exampl.com>
2022-07-17 17:35:35 +0300

    Added another solution

--- src/task 5/task5.js
+++ src/task 5/task5.js
@@ -1,19 +1,12 @@
     
 function range(start, end, step){
     var result = [];
-        (step == undefined)? step = 1:num = Math.abs((end - start)/ step )+1;
-    if(start>end){
-        for(var i=end; i<=num; i++){
-        result.push(start);
-        start = start + step;
+        (step == undefined)? step = 1 : num = Math.abs((end - start)/ step)+1;
+    (start>end)? i=end-1 : i=start;
+        for(i; i<=num; i++){
+            result.push(start);
+            start = start + step;
         }
-    }
-    else {
-        for(var i=start; i<=num; i++){
-        result.push(start);
-        start = start + step;
-        }
-    }
     return result;
 }
 
