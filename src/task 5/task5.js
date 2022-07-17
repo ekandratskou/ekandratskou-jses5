b646967ab9bc0b89ce69b9e66a27a7469065af01
Eduard <eduard@exampl.com>
2022-07-17 14:38:14 +0300

    Task5 fixed

--- src/task 5/task5.js
+++ src/task 5/task5.js
@@ -1,32 +1,25 @@
-function range(start, end){
-    var result = [];
-    for(i=start; i<=end; i++){
-        result.push(i);
-    }
-    return result;
-}
-
-            
-function sum(array){
-    var result = 0;
-    for(j=0; j<array.length; j++){
-        result = result + array[j];
-    }
-return result;
-}
-
-
+    
 function range(start, end, step){
     var result = [];
-    if(step == undefined){
-        step = 1;
-    }
-    var num = Math.abs((end / step )-1);
-    for (var i = 0; i<= num; i++){
+        (step == undefined)? step = 1:num = Math.abs((end - start)/ step )+1;
+    if(start>end){
+        for(var i=end; i<=num; i++){
         result.push(start);
         start = start + step;
+        }
+    }
+    else {
+        for(var i=start; i<=num; i++){
+        result.push(start);
+        start = start + step;
+        }
     }
     return result;
 }
 
-            
+
+ function sum(){
+     var result = arr.reduce((x, y) => x + y);
+     return result;
+}  
+
