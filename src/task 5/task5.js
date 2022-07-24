123f00728009c92d8a0b8faec9fe72f79ad9d484
Eduard Kandratskou <EKandratskou@ibagroup.eu>
2022-07-24 14:55:28 +0300

    Added another solution

--- src/task 5/task5.js
+++ src/task 5/task5.js
@@ -1,18 +1,12 @@
     
 function range(start, end, step){
     var result = [];
-        (step == undefined)? step = 1 : num = Math.abs((end - start)/ step)+1;
-    (start>end)? i=end-1 : i=start;
-        for(i; i<=num; i++){
+    var num = Math.abs((end - start)/ step)+1;
+        step = (undefined)?  1 :  step;
+        for(i = (start<end)? i=start : i=end-1; i<=num; i++){
             result.push(start);
             start = start + step;
         }
     return result;
 }
-
-
- function sum(){
-     var result = arr.reduce((x, y) => x + y);
-     return result;
-}  
 
