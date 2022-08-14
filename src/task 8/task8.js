91a112a64021d68c334f7973d17ba9d2643542e9
Eduard <eduard@exampl.com>
2022-08-14 23:52:54 +0300

    Added another solution task8

--- src/task 8/task8.js
+++ src/task 8/task8.js
@@ -2,27 +2,18 @@ function deepEqual(val_1, val_2) {
     if (val_1 === val_2){
         return true;
     }
-    if (val_1 == null || val_2 == null){
+    if (val_1 == null || val_2 == null || typeof val_1 != 'object' || typeof val_2 != 'object'){
         return false; 
      }  
-    if (typeof val_1 != 'object' || typeof val_2 != 'object'){
-        return false;
+                 
+   for (var prop in val_1) {
+    if (val_2.hasOwnProperty(prop)) {
+        if (! deepEqual(val_1[prop], val_2[prop]) )
+            return false; 
+    } else {
+        return false; 
     }
-             
-    var obj_1 = 0;
-    var obj_2 = 0;
-
-    for(var char in val_1){
-          obj_1 = obj_1 +1;  
-        }
-    for(var char in val_2){
-          obj_2 = obj_2 +1;  
-        if(!(char in val_1) || !deepEqual( val_1[char], val_2[char] )){
-            return false;
-        }
-    }
-        return obj_1 === obj_2;
-        
- }
-
+   }
+return true;
+}
 
