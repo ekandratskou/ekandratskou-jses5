d28a5f00634ea85b5e21d26427366ed2191f5ff8
Eduard <eduard@exampl.com>
2022-09-01 23:18:30 +0300

    I really hope it's the latest commit of task8 :)

--- src/task 8/task8.js
+++ src/task 8/task8.js
@@ -1,5 +1,4 @@
 function deepEqual(val_1, val_2) {
-    if (val_1 === null && val_2 === null){return false;}
     if (val_1 === val_2){return true;}
     if (typeof val_1 != "object" || val_1 == null || typeof val_2 != "object" || val_2 == null) {return false;}
     if (Object.keys(val_1).length != Object.keys(val_2).length){return false;}
@@ -7,6 +6,6 @@ function deepEqual(val_1, val_2) {
    for (var prop in val_2) {
     if (!(val_1.hasOwnProperty(prop)) || !deepEqual(val_1[prop], val_2[prop])) {
         return false;
-    } return true;
-   }
+    } 
+   }return true;
 }
