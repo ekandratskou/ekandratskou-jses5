4b2e1b99cd54a58844fdc680cdda5811c537e13e
Eduard <eduard@exampl.com>
2022-10-06 23:22:23 +0300

    Added solution task12

--- src/task 12/task12.js
+++ src/task 12/task12.js
@@ -1,6 +1,6 @@
 function some(arr, arg){
-    return arr.filter(function(num){return !arg(num)}).length != arr.length;
+    return !(Object.keys(arr.filter(function(num){return !arg(num)})).length === Object.keys(arr).length);
 }
 function every(arr, arg){
-   return arr.filter(function(num){return arg(num)}).length === arr.length;
+   return Object.keys(arr.filter(function(num){return arg(num)})).length === Object.keys(arr).length;
 }
