970776bacf60d886c3489b9b433f872f491b12be
Eduard <eduard@exampl.com>
2022-10-11 22:06:21 +0300

    Seventh versions of the solution of the task 12

--- src/task 12/task12.js
+++ src/task 12/task12.js
@@ -1,6 +1,6 @@
 function some(arr, arg){
-    return !(Object.keys(arr.filter(function(num){return !arg(num)})).length === Object.keys(arr).length);
+    return !(arr.filter(function(num){return !arg(num)}).length === Object.keys(arr).length);
 }
 function every(arr, arg){
-   return Object.keys(arr.filter(function(num){return arg(num)})).length === Object.keys(arr).length;
+   return arr.filter(function(num){return arg(num)}).length === Object.keys(arr).length;
 }
