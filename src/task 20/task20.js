f1d7793667de2a060e49ad14afa2b44618ad2b2f
Eduard <eduard@exampl.com>
2022-12-02 01:00:16 +0300

    Added another solution task20

--- src/task 20/task20.js
+++ src/task 20/task20.js
@@ -1,10 +1,10 @@
 // Fill in this regular expression.
-var number = /^([\+|\-]?(\d+)(\.)?(\d+)?([e|E])?(\d+|\+|\-)?(\d+)?)$/;
+var number = /^([\-|\+|\~]?\d+(\.?)(\/?)(\d+)?(m[+|\-])?(e[-|+]?\d+)?(k)?|[-|+]?\.(\d+)(e[-|+]?\d+)?)$/i;
 
 // Tests:
 function doTestNumber() {
     ["1", "-1", "+15", "1.55", ".5", "5.", "1.3e2", "1E-4",
-     "1e+12"].forEach(function(s) {
+     "1e+12", '3k', '3K', '2.2m+', '2.2M-', '1/4', '001010', '~7'].forEach(function(s) {
       if (!number.test(s)) {
           console.log("Failed to match '" + s + "'");
           return false;
@@ -19,4 +19,19 @@ function doTestNumber() {
     });
 }
 
+function validator(){
+const atLeast8characters = /^.{8,}$/
+const latinCharacters = /^[a-z]+$/i;
+const numBer = /^\d+$/
+const symbol = /^\s+$/
+var password = "12345"
 
+if( atLeast8characters.test(password) &&
+    latinCharacters.test(password)&& 
+    numBer.test(password)&&
+    symbol.test(password) ){
+    console.log('ok');
+} else {
+    console.log('wrong');
+}
+};
