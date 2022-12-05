69a02a85b8bf40829c0f314b6c48d553e40fb856
Eduard <eduard@exampl.com>
2022-12-05 21:09:50 +0300

    Modified task20

--- src/task 20/task20.js
+++ src/task 20/task20.js
@@ -5,33 +5,26 @@ var number = /^([\-|\+|\~]?\d+(\.?)(\/?)(\d+)?(m[+|\-])?(e[-|+]?\d+)?(k)?|[-|+]?
 function doTestNumber() {
     ["1", "-1", "+15", "1.55", ".5", "5.", "1.3e2", "1E-4",
      "1e+12", '3k', '3K', '2.2m+', '2.2M-', '1/4', '001010', '~7'].forEach(function(s) {
-      if (!number.test(s)) {
-          console.log("Failed to match '" + s + "'");
-          return false;
+        if (!number.test(s)) {
+            console.log("Failed to match '" + s + "'");
+            return false;
       }
     });
     ["1a", "+-1", "1.2.3", "1+1", "1e4.5", ".5.", "1f5",
      "."].forEach(function(s) {
-      if (number.test(s)) {
-          console.log("Incorrectly accepted '" + s + "'");
-          return false;
+        if (number.test(s)) {
+            console.log("Incorrectly accepted '" + s + "'");
+            return false;
       }
     });
 }
 
 function validator(){
-const atLeast8characters = /^.{8,}$/
-const latinCharacters = /^[a-z]+$/i;
-const numBer = /^\d+$/
-const symbol = /^\s+$/
-var password = "12345"
-
-if( atLeast8characters.test(password) &&
-    latinCharacters.test(password)&& 
-    numBer.test(password)&&
-    symbol.test(password) ){
-    console.log('ok');
-} else {
-    console.log('wrong');
-}
+const characters = /(?=.*[a-z])(?=.*[\d+])(?=.*[~№!@#$%^&*])[(a-z)(\d+)(~№!@#$%^&*)]{8,}/gi;
+var password = "!WinterIsComing123";
+    if(characters.test(password)){
+        console.log('ok');
+    } else {
+        console.log('wrong');
+    }
 };
