9db9a86fe16514e944d3369762daea8a9d062970
Eduard <eduard@exampl.com>
2022-12-14 01:14:44 +0300

    Modified solutioun task22

---
+++ src/task 22/task22.js
@@ -0,0 +1,30 @@
+function buildTable(arr){
+    var Table = document.querySelector("#table");
+    var headers = Object.keys(arr[0]);
+    var table =document.createElement("table");
+    var headerRow = document.createElement("tr");
+    headers.forEach(function (headerText){
+        var header = document.createElement("th");
+        header.textContent = headerText;
+        headerRow.appendChild(header);
+    });
+    table.appendChild(headerRow);
+    arr.forEach(function(X){
+        var row = document.createElement("tr")
+        Object.values(headers).forEach(function(headerText){
+            var cell = document.createElement("td");
+            if(typeof X[headerText] == "number"){
+                cell.style.textAlign = "right"
+            }else if(X[headerText] == " "){
+                X[headerText] = "-"
+            }
+            cell.textContent= X[headerText];
+            row.appendChild(cell);
+        })
+        table.appendChild(row);
+    })
+    Table.appendChild(table);
+}
+
+       
+    
