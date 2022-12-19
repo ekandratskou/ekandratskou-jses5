2fb70f590618674fb3ebc355e93a86dec45b521a
Eduard <eduard@exampl.com>
2022-12-19 23:31:17 +0300

    Modified2 solution task22

--- src/task 22/task22.js
+++ src/task 22/task22.js
@@ -1,6 +1,6 @@
 function buildTable(arr){
     var Table = document.querySelector("#table");
-    var headers = Object.keys(arr[0]);
+    var headers = Object.keys(arr[1]);
     var table =document.createElement("table");
     var headerRow = document.createElement("tr");
     headers.forEach(function (headerText){
@@ -15,8 +15,9 @@ function buildTable(arr){
             var cell = document.createElement("td");
             if(typeof X[headerText] == "number"){
                 cell.style.textAlign = "right"
-            }else if(X[headerText] == " "){
-                X[headerText] = "-"
+            }else if(X[headerText] == undefined){
+                X[headerText]  = "-";
+                cell.style.textAlign = "center";
             }
             cell.textContent= X[headerText];
             row.appendChild(cell);
