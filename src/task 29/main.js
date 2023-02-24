18aaa70e28011e597f96262739b7aff867609f89
Eduard Kandratskou <EKandratskou@ibagroup.eu>
2023-02-24 13:14:17 +0300

    Modified the solution of task29

--- src/task 29/main.js
+++ src/task 29/main.js
@@ -2,27 +2,64 @@ var results = [
 {name: "Удовлетворён", count: 1043, color: "lightblue"},
 {name: "Нейтральное", count: 563, color: "lightgreen"},
 {name: "Не удовлетворён", count: 510, color: "pink"},
-{name: "Без комментариев", count: 175, color: "silver"}
+{name: "Не удовлетворён", count: 150}
 ];
 var cx = document.querySelector("canvas").getContext("2d");
-var total = results.reduce(function(sum, choice) {
-return sum + choice.count;
-}, 0);
-var currentAngle = -0.5 * Math.PI;
-var centerX = 300, centerY = 150;
-results.forEach(function(result) {
-    var sliceAngle = ((result.count / total) * 2 * Math.PI);
-    cx.beginPath();
-    cx.font = "16px Georgia"
-    cx.arc(centerX, centerY, 100, currentAngle, currentAngle + sliceAngle);
-    currentAngle += sliceAngle;
-    cx.lineTo(centerX, centerY);
-    cx.fillStyle = "black";
-    var anglText = currentAngle - (sliceAngle/2) ;
+
+function chartPie(R, centerX, centerY){
+    var total = results.reduce(function(sum, choice) {
+        if(choice.count == undefined){
+            choice.count = 0;
+        };
+        return sum + choice.count;
+    }, 0);
+    var currentAngle = -0.5 * Math.PI;
+    var red = RandomColor(255);
+    var green = RandomColor(255);
+    var blue = RandomColor(255);
+    
+    function RandomColor(num) {
+        return Math.floor(Math.random() * Math.floor(num));
+    };
+    //Legend results.hasOwnProperty(‘name’) 
+    for (i=0; i<results.length; i++){
+        if(results[i].hasOwnProperty("name") == false) {
+            results[i].name = "-"
+        };
+        if(results[i].hasOwnProperty("color") == false) {
+            cx.fillStyle = "rgb(" + red + "," + green + "," + blue + ")"
+        };
+        cx.fillStyle = results[i].color;
+        cx.fill();
+        cx.fillRect(centerX - 250, centerY - 50 +(i * 30), 10, 10);
+        cx.fillStyle = "black";
+        cx.fillText(results[i].name + "   " + results[i].count, centerX - 230, centerY - 50 + ((i * 30)+10));
+    };
+    results.forEach(function(result) {
+        var sliceAngle = ((result.count / total) * 2 * Math.PI);
+        cx.beginPath();
+        cx.arc(centerX, centerY, R, currentAngle, currentAngle + sliceAngle);
+        currentAngle += sliceAngle; console.log(R)
+        cx.lineTo(centerX, centerY);
+        if(result.color == undefined) {
+            cx.fillStyle = "rgb(" + red + "," + green + "," + blue + ")"
+        };
+        cx.fillStyle = result.color;
+        cx.fill();
+        cx.font = "16px Georgia"
+        cx.fillStyle = "black";
+        var anglText = currentAngle - (sliceAngle/2) ;
     //полярные координаты: x = R* cos(angle), y = R* sin(angle)
-    cx.fillText(result.name, centerX + (120 * Math.cos(anglText)), centerY + (120 * Math.sin(anglText)));
-    cx.textAlign = "right";
-    cx.textBaseline= "middle";
-    cx.fillStyle = result.color;
-    cx.fill();
-});
+        var labelText = Math.round(R * result.count /total);
+        if(result.count>0){
+        cx.fillText(labelText + "%", centerX + (60 * Math.cos(anglText)), centerY + (60 * Math.sin(anglText)));
+        cx.textAlign = "center";
+        cx.textBaseline= "middle";
+        }
+        if(result.count == undefined){
+            labelText = "-"
+        };
+    });
+};
+
+//chartPie(100, 300, 150);
