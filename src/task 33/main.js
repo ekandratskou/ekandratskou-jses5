d8365696c34e9123d767105faab950a03047e407
Eduard <eduard@exampl.com>
2023-03-21 00:27:04 +0300

    Modified the solution of task33 v2.0

--- src/task 33/main.js
+++ src/task 33/main.js
@@ -1,15 +1,15 @@
-var butt = document.getElementById("button");
-var tArea = document.getElementById("code");
-var pre = document.getElementById("output")
-
-
-function workbench(){
-    return new Promise(function(success) {
+function workbench(butt, tArea, pre){
+    function helper(){
+        return new Promise(function(success) {
         var func = new Function(tArea.value);
         var result = func();
         success(pre.textContent = String(result));
        }).catch(function(err){pre.textContent = "Error: " + err;})  
 };
+    butt.addEventListener("click", helper);
+  
+};
 
-butt.addEventListener("click", workbench)
-
+  workbench(document.getElementById("button"), 
+            document.getElementById("code"), 
+            document.getElementById("output"));
